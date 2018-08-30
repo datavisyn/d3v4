a5f47b6b8feb32dcf2e6cff9f432ffd32837eebd
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-08-30 08:22:22 +0200

    update to 5.7.0

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ export * from "d3-queue";
 export * from "d3-random";
 export * from "d3-request";
 export * from "d3-scale";
+export * from "d3-scale-chromatic";
 export * from "d3-selection";
 export * from "d3-shape";
 export * from "d3-time";
